a9eb0652188e9eccc35c8d2f3f1b08fbed2017a3
Skyeyezz <113293306+Skyeyezz@users.noreply.github.com>
2023-05-21 15:35:12 +0200

    Update playlistDownloader.sh

--- playlistDownloader.sh
+++ playlistDownloader.sh
@@ -4,38 +4,23 @@
 # downloads videos to a directory without removing existing files as well as writing video names of playlist to a .txt file
 # the purpose of writing the .txt file is to compare previous downloads to new downloads and see if any videos have been removed by youtube, etc
 
-# omit <> when filling in links!
-# but do not remove ""
-
-# just copy paste the code if you have multiple playlists, different directories and such
-
 set -euo pipefail
 
 start=$(date +%s)
 
 download_dir="your download directory"
-destination_dir="the destination directory"
+playlist_dir="your playlist directory"
 
 cd "$download_dir"
-# omit -x if you do not want only the audio
-yt-dlp -x --playlist-start <number> <playlist / video link>
+yt-dlp -x --playlist-start 1 "playlist link"
 
-# change the /*.opus in the loop if you want any other types of files
+rsync -av --remove-source-files "$download_dir/" "$playlist_dir/"
 
-for file in "$download_dir"/*.opus; do
-  filename=$(basename "$file")
-  if [ -f "$destination_dir/$filename" ]; then
-    rm "$file"
-  else
-    mv "$file" "$destination_dir/"
-  fi
-done
+# write names for playlist to .txt file
 
-# write names of music into .txt file
+outputted_file_name="playlist"
 
-base_output_file="music" # preferably make this the name of your playlist
-
-output_directory="<your output directory>" # where the .txt file will be outputted
+output_directory="your output directory"
 
 counter=1
 while true; do
@@ -46,7 +31,7 @@ while true; do
   ((counter++))
 done
 
-cd "$destination_dir"
+cd "$playlist_dir"
 
 for file in *
 do
@@ -59,8 +44,8 @@ echo "FINISHED EXECUTING SCRIPT"
 end=$(date +%s)
 elapsed=$(( end - start ))
 printf "Time Taken: %d seconds.\n" "$elapsed"
+printf "Time Taken: %.1f minutes.\n" $(echo "$elapsed / 60" | bc -l)
 
-# you can use this command to write the name of videos in a playlist into a .txt file without an existing directory of downloaded videos
-# do not omit the > here:
+# you can use this command to write the name of videos in a playlist into a .txt file without an existing directory of downloaded videos:
 # yt-dlp --get-title --no-playlist "playlist / video link" > playlist.txt
 
